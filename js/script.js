bb2a05df8e0b78125daff19186107271ffaafd79
samarthao <122334992+samarthao@users.noreply.github.com>
2023-04-17 15:20:37 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ document.querySelector("#submit").addEventListener("click",function welcome () {
 	var fname = document.getElementById("fname").value;
     var favcolor = document.getElementById("favcolor").value;
     var suphero = document.getElementById("suphero").value;
-    var btn = document.createElement("INPUT");
-        btn.setAttribute("type", "button");
-        btn.setAttribute("value", "Continue");
+    // var btn = document.createElement("INPUT");
+    //     btn.setAttribute("type", "button");
+    //     btn.setAttribute("value", "Continue");
     // var msg = document.getElementById("welcome_msg");
     document.getElementById("welcome_msg").style.backgroundColor = favcolor;
     document.querySelector("#welcome_msg").innerHTML = "Welcome, " + fname + "! " + favcolor + " is an awesome color " + " and " + suphero + " is a great superhero " + "! Spider-Man is my favorite hero & he has the coolest super power Spidey Senses! Continue to see how you can activate your own Spidey Senses";
